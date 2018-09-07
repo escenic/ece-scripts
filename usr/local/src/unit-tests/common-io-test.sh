escenic/ece-scripts

commit 3e55e3d215659dec2f091df603fcf1d73ee657cc

make_dir now can handle file or directory path with or without space
- update make_dir
- added unit-test script usr/local/src/unit-tests/common-io-test.sh

Adding double quote for file or directory name

- updated function remember_state

usr/share/escenic/ece-scripts/common-io.sh
@@ -63,11 +63,9 @@ function set_conf_file_value() {
 ##
 ## $@ :: a list of directories
 function make_dir() {
-  for el in $@; do
-    if [ ! -d $el ]; then
-      run mkdir -p $el
-    fi
-  done
+  while read -r el; do
+    mkdir -p "${el}"
+  done <<< "${@}"
 }
 
 ### remove_dir

usr/local/src/unit-tests/common-io-test.sh
@@ -1,0 +1,41 @@
+#! /usr/bin/env bash
+## author: mah@escenic.com
+
+test_can_determine_make_dir_works_with_space() {
+  local tmp_dir=
+  tmp_dir=$(mktemp -d)
+  local file_path="${tmp_dir}/test website"
+  local expected=0
+  local actual=
+  make_dir ${file_path} && actual=$? || actual=$?
+  rm -rf ${tmp_dir}
+  assertEquals "Can determine make_dir works with space" "${expected}" "${actual}"
+}
+
+test_can_determine_make_dir_works_without_space() {
+  local tmp_dir=
+  tmp_dir=$(mktemp -d)
+  local file_path="${tmp_dir}/testwebsite"
+  local expected=0
+  local actual=
+  make_dir ${file_path} && actual=$? || actual=$?
+  rm -rf ${tmp_dir}
+  assertEquals "Can determine make_dir works without space" "${expected}" "${actual}"
+}
+
+## @OVERRIDE shunit2
+setUp() {
+  source "$(dirname "$0")/../../../share/escenic/ece-scripts/common-bashing.sh"
+  source "$(dirname "$0")/../../../share/escenic/ece-scripts/common-io.sh"
+}
+
+## @override shunit2
+tearDown() {
+  :
+}
+
+main() {
+  . "$(dirname "$0")"/shunit2/shunit2
+}
+
+main "$@"

.gitignore
@@ -1,3 +1,4 @@
 *.dockerfile.test
 *.ignore
 usr/local/src/unit-tests/shunit2
+*.idea

usr/sbin/ece-deploy
@@ -504,18 +504,18 @@ function remember_state() {
   print_and_log "Remembering all files from the current version of" \
     vosa-conf-$HOSTNAME "..."
   dpkg -L vosa-conf-$HOSTNAME | while read f; do
-    local dir=${1}/vosa-conf-${HOSTNAME}/$(dirname $f)
+    local dir=${1}/vosa-conf-${HOSTNAME}/$(dirname "$f")
     make_dir $dir
-    if [ -d $f ]; then
+    if [ -d "$f" ]; then
       continue
     fi
-    if [ ! -e $f ]; then
-      print_and_log $(yellow WARNING) $f "was installed with the" \
+    if [ ! -e "$f" ]; then
+      print_and_log $(yellow WARNING) "$f" "was installed with the" \
         "vosa-conf-$HOSTNAME package," \
         "however, it's not present on $HOSTNAME."
       continue
     fi
-    cp $f $dir/
+    cp "$f" "$dir/"
   done
 }
 

commit f5535e6bf8f29407192f2b539b01633d5c63bbf8

Fix regression in common-io;:make_dir wrt multiple directories
- make_dir was extended to support directories with spaces in them. This
  broke calls to the function where there were multiple directories passed
  to it. This has now been fixed.

- added more unit tests to prove that both directories with spaces and
  multiple directories work, both checking the actual call to make_dir as
  well as listing the contents of the created directories.

- the change requires that directories passed to the function should be
  quoted, especially if they may contain spaces.

- regression bug introduced in 3e55e3d215659dec2f091df603fcf1d73ee657cc

usr/share/escenic/ece-scripts/common-io.sh
@@ -63,9 +63,9 @@ function set_conf_file_value() {
 ##
 ## $@ :: a list of directories
 function make_dir() {
-  while read -r el; do
+  for el in "$@"; do
     mkdir -p "${el}"
-  done <<< "${@}"
+  done
 }
 
 ### remove_dir

usr/share/escenic/ece-scripts/ece-install.d/app-server.sh
@@ -2,7 +2,7 @@ function set_up_jdbc_library() {
   print_and_log "Setting up the jdbc driver"
   if [ -n "$jdbc_driver" -a -e "$jdbc_driver" ]; then
     make_ln $jdbc_driver
-  elif [ ! -z "$db_vendor" ] ; then
+  elif [ -n "$db_vendor" ] ; then
     local jdbc_url=${fai_jdbc_url-${default_jdbc_url}}
     print_and_log "Downloading jdbc driver ${jdbc_url}"
     local jdbc_jar=${jdbc_url##*/}

usr/local/src/unit-tests/common-io-test.sh
@@ -7,9 +7,13 @@ test_can_determine_make_dir_works_with_space() {
   local file_path="${tmp_dir}/test website"
   local expected=0
   local actual=
-  make_dir ${file_path} && actual=$? || actual=$?
+  make_dir "${file_path}" && actual=$? || actual=$?
+  assertEquals "Can determine make_dir works with space (call)" "${expected}" "${actual}"
+
+  ls "${file_path}" &>/dev/null  && actual=$? || actual=$?
+  assertEquals "Can list results of make_dir with names with space " "${expected}" "${actual}"
+
   rm -rf ${tmp_dir}
-  assertEquals "Can determine make_dir works with space" "${expected}" "${actual}"
 }
 
 test_can_determine_make_dir_works_without_space() {
@@ -18,9 +22,27 @@ test_can_determine_make_dir_works_without_space() {
   local file_path="${tmp_dir}/testwebsite"
   local expected=0
   local actual=
-  make_dir ${file_path} && actual=$? || actual=$?
+  make_dir "${file_path}" && actual=$? || actual=$?
   rm -rf ${tmp_dir}
   assertEquals "Can determine make_dir works without space" "${expected}" "${actual}"
+}
+
+test_can_create_multiple_directories () {
+  local tmp_dir=
+  tmp_dir=$(mktemp -d)
+  local file_path="${tmp_dir}/testwebsite"
+  local file_path2="${tmp_dir}/another-website"
+  local expected=0
+  local actual=
+  make_dir ${file_path} ${file_path2} && actual=$? || actual=$?
+  assertEquals "Can create multiple directories" "${expected}" "${actual}"
+
+  ls "${file_path}" &>/dev/null  && actual=$? || actual=$?
+  assertEquals "Can create multiple directories" "${expected}" "${actual}"
+  ls "${file_path2}" &>/dev/null  && actual=$? || actual=$?
+  assertEquals "Can create multiple directories" "${expected}" "${actual}"
+
+  rm -rf ${tmp_dir}
 }
 
 ## @OVERRIDE shunit2